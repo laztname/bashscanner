f6a93fd86b369b1414505d972854bfe03efbf5f8
lazt <joniasfortuna1@gmail.com>
2018-07-05 23:56:26 +0700

    fix typo xD

--- scan.sh
+++ scan.sh
@@ -2,4 +2,4 @@
 #you can change ip ex localhost change to 192.168.1.1
 #regradz laztname
 
-for i in {1..100}; do nc -z localhost $i && echo "port $i opened" || echo "port $i closed" | grep buka; done
+for i in {1..100}; do nc -z localhost $i && echo "port $i opened" || echo "port $i closed" | grep opened; done
